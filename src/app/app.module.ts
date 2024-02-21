bea2d6339c9444f4eaa8e933f2beb25a73f6820f
Martin Wilches <martindotdev@gmail.com>
2024-02-21 14:30:47 -0500

    Componentes mas pequeños en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component'
 import { HeroesModule } from './heroes/heroes.module'
+import { DbzModule } from './dbz/dbz.module'
 
 @NgModule({
     declarations: [AppComponent], // Definicion de los componentes que podran ser accedidos dentro del modulo
-    imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
+    imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule, DbzModule], // La importacion de los modulos incluye los componentes cada uno estos contenga
     providers: [],
     bootstrap: [AppComponent]
 })
---
+++ src/app/dbz/components/form/form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'dbz-form',
+    templateUrl: './form.component.html',
+    styleUrl: './form.component.css'
+})
+export class FormComponent {}
